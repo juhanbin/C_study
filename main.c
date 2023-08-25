339be9638ae52d6e38823f216d16075afe077c2a
juhanbin <gksqls0509@naver.com>
2023-08-25 10:46:38 +0900

    산술 연산자를 사용하여 사칙 연산하기

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 
  void main()
  {
-     int data1,data2;
-     data1 = 5;
-     data2 = data1;
-     printf("data1 = %d, data2 = %d\n",data1,data2);
+     int data1 = 5, data2 = 3;
+
+     int result1 = data1 + data2;
+     int result2 = data1 * data2;
+     int result3 = data1 / data2;
+     int result4 = data1 % data2;
+
+     printf("result : %d, %d, %d, %d\n", result1, result2, result3, result4);
  }

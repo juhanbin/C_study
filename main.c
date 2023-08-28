ffdbaf7ca15ed7e45add004bca6e51a507d35c3e
juhanbin <gksqls0509@naver.com>
2023-08-28 14:42:28 +0900

    while 반복문을 사용하여 1부터 5까지 더하기

--- main.c
+++ main.c
@@ -5,13 +5,12 @@
      int sum=0;
      int num=1;
 
-     for( ; ; )
+     while(num<=5)
      {
          printf("num(%d) + sum(%d) =",num,sum);
          sum = sum +num;
          printf("%d\n",sum);
          num++;
-         if(num>5) break;
      }
      printf("\nResult : num = %d sum=%d",num,sum);
  }

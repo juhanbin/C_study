20a6bfc5a3bff64c787845dc0586a69c3ea2a5ba
juhanbin <gksqls0509@naver.com>
2023-08-29 11:53:59 +0900

    변수의 특정 비트를 0으로 만드는 예제

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
- void main()
+ unsigned char RestBit(unsigned char dest_data,unsigned char bit_num)
  {
-    int m;
-    for(m=5;m<8;m++)
-    {
-        if(m==6)continue;
-        printf("m(%d)\n",m);
-    }
+    if(bit_num < 8) dest_data=dest_data & ~(0x01 << bit_num);
+    return dest_data;
+ }
+ int main()
+ {
+  unsigned char lamp_staatus = 0x7F;
+     printf("%x->",lamp_staatus);
+     lamp_staatus = RestBit(lamp_staatus,3);
+     printf("%x\n",lamp_staatus);
  }

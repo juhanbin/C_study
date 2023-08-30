f04ee57138397b8d0aa42b64dbe526ede62baa62
juhanbin <gksqls0509@naver.com>
2023-08-30 14:46:34 +0900

    변수 사용 범위에 따른 유효성 이해하기

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
- unsigned char SetBit(unsigned char dest_data,unsigned char bit_num)
+ int Sum(int data1, int data2)
  {
-    if(bit_num < 8) dest_data=dest_data | (0x01 << bit_num);
-    return dest_data;
+    int result = data1 + data2;
+     return result;
  }
  int main()
  {
-  unsigned char lamp_stat = 0x77;
-     printf("%x->",lamp_stat);
-     lamp_stat = SetBit(lamp_stat,3);
-     printf("%x\n",lamp_stat);
+    int result;
+    result = Sum(5,3);
+     printf("5 + 3 = %d\n",result);
  }

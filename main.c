98b0f066bfb63bde67a05c3a274ea2f34c252191
juhanbin <gksqls0509@naver.com>
2023-08-30 12:49:04 +0900

    변수의 특정비트를 1로 만드는 예제

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
- unsigned char RestBit(unsigned char dest_data,unsigned char bit_num)
+ unsigned char SetBit(unsigned char dest_data,unsigned char bit_num)
  {
-    if(bit_num < 8) dest_data=dest_data & ~(0x01 << bit_num);
+    if(bit_num < 8) dest_data=dest_data | (0x01 << bit_num);
     return dest_data;
  }
  int main()
  {
-  unsigned char lamp_staatus = 0x7F;
-     printf("%x->",lamp_staatus);
-     lamp_staatus = RestBit(lamp_staatus,3);
-     printf("%x\n",lamp_staatus);
+  unsigned char lamp_stat = 0x77;
+     printf("%x->",lamp_stat);
+     lamp_stat = SetBit(lamp_stat,3);
+     printf("%x\n",lamp_stat);
  }

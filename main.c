3d2301af1a2f6d73d7d3448e8bebe248441dbdf1
juhanbin <gksqls0509@naver.com>
2023-08-30 15:53:31 +0900

    배열의 특정 요소에 값대입하기

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
- int result;
-
- int Sum(int data1, int data2)
- {
-     int result;
-    result = data1 + data2;
- }
  int main()
  {
-    Sum(5,3);
-     printf("5 + 3 = %d\n",result);
+    short student[20];
+
+    student[1] = 10;
+     printf("%d\n",student[1]);
  }

17c483879534f04edd06ac47e51b56ee0c484a9f
juhanbin <gksqls0509@naver.com>
2023-08-31 10:20:57 +0900

    배열의 각 요소에 저장된 값 합하기

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 
  int main()
  {
-    short student[20]={0,};
-
-    student[1] = 10;
-     printf("%d %d\n",student[1],student[2]);
+    char data[5] = {1,2,3,4,5};
+    int result = 0,i;
+    for(i=0;i<5;i++)
+    {
+        result = result + data[i];
+    }
+     printf("data 배열의 각 요소의 합은 %d입니다\n",result);
  }

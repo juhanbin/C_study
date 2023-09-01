091c1b83e4723298439c716ffad470b389ae16b7
juhanbin <gksqls0509@naver.com>
2023-09-01 15:25:18 +0900

    strlen 함수를 사용하여 문자열 길이 출력하기

--- main.c
+++ main.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
- int GetStringLength(char data[])
- {
-    int count = 0;
-
-     while (data[count])
-         count++;
-     return count;
- }
  int main()
  {
     int data_length;
-    char data[10] = {'h','a','p','p','y',0};
-
-    data_length = GetStringLength(data);
-     printf("data length = %d\n",data_length);
+    char data[10] = {'h','a','p','p','y',};
+    data_length = strlen(data);
+     printf("data lenth = %d\n",data_length);
  }

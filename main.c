39c256bbc9878c1216457b97d47ca0d1dd8f7db7
juhanbin <gksqls0509@naver.com>
2023-09-04 15:46:59 +0900

    2차원 배열을 사용하여 바둑판정보를 저장하고 알려주기

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 
  int main()
  {
-    char data[12] = {0,0,2,0,1,1,0,0,2,1,0,2};
-    int i,x,y;
-    for(i = 0;i<12;i++)
+    char data[3][4] = {{0,0,2,0},{1,1,0,0},{2,1,0,2}};
+    int x,y;
+    for(y = 0; y<3; y++)
     {
-        x = i % 4 + 1;
-        y = i / 4 + 1;
-        printf("%d행 %d열에",y,x);
-        if (data[i] == 1) printf("검은돌이 놓여 있습니다.\n");
-        else if (data[i] == 2) printf("흰돌이 놓여 있습니다.\n");
-        else printf("는 돌이 놓여 있지 않습니다.\n");
+        for (x=0; x<4; x++)
+        {
+            printf("%d행 %d열에",y+1,x+1);
+            if (data[y][x] == 1) printf("검은돌이 놓여 있습니다.\n");
+            else if (data[y][x] == 2) printf("흰돌이 놓여 있습니다.\n");
+            else printf("는 돌이 놓여 있지 않습니다.\n");
+        }
     }
  }

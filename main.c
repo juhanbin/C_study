f388cc2e93c7755e1f82a55d34e6557afb152538
juhanbin <gksqls0509@naver.com>
2023-09-04 15:42:51 +0900

    1차원 배열을 사용하여 바둑판 정보를 저장하고 알려주기

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <string.h>
 
  int main()
  {
-    char data[10] = {'a','b','c',0};
-    char result[16];
-
-    strcpy(result,data);
-    strcat(result,"def");
-
-     printf("%s + \"def\" = %s\n",data, result);
+    char data[12] = {0,0,2,0,1,1,0,0,2,1,0,2};
+    int i,x,y;
+    for(i = 0;i<12;i++)
+    {
+        x = i % 4 + 1;
+        y = i / 4 + 1;
+        printf("%d행 %d열에",y,x);
+        if (data[i] == 1) printf("검은돌이 놓여 있습니다.\n");
+        else if (data[i] == 2) printf("흰돌이 놓여 있습니다.\n");
+        else printf("는 돌이 놓여 있지 않습니다.\n");
+    }
  }

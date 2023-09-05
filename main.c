7a17c937e65aca8f065f0e7a2c1901ff76a11970
juhanbin <gksqls0509@naver.com>
2023-09-05 20:20:01 +0900

    직접 주소 지정 방식으로 변수 값 교환하기

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+ void Swap(int a, int b)
+ {
+    int temp = a;
+    a=b;
+    b=temp;
+ }
+
  int main()
  {
-    short birthday;
-    short *ptr;
-    ptr = &birthday;
+    int start = 96, end = 5;
 
-     printf("birthday 변수의 주소는 %p입니다.\n",ptr);
+     printf("before : start = %d, end = %d\n",start,end);
+     if(start > end)
+     {
+         Swap(start,end);
+     }
+     printf("after : start = %d, end = %d\n",start,end);
  }

7fc168cf3e7a1bb17420fe4f6586dd3a12d3afe0
juhanbin <gksqls0509@naver.com>
2023-09-05 19:59:39 +0900

    변수가 위치한 메모리 주소 출력하기

--- main.c
+++ main.c
@@ -2,16 +2,9 @@
 
  int main()
  {
-    char data[3][4] = {{0,0,2,0},{1,1,0,0},{2,1,0,2}};
-    int x,y;
-    for(y = 0; y<3; y++)
-    {
-        for (x=0; x<4; x++)
-        {
-            printf("%d행 %d열에",y+1,x+1);
-            if (data[y][x] == 1) printf("검은돌이 놓여 있습니다.\n");
-            else if (data[y][x] == 2) printf("흰돌이 놓여 있습니다.\n");
-            else printf("는 돌이 놓여 있지 않습니다.\n");
-        }
-    }
+    short birthday;
+    short *ptr;
+    ptr = &birthday;
+
+     printf("birthday 변수의 주소는 %p입니다.\n",ptr);
  }

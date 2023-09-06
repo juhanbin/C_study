c5714c9f8a9ae7052ca4658130f097c4af6d5e49
juhanbin <gksqls0509@naver.com>
2023-09-06 15:48:57 +0900

    getchar 함수를 사용하여 키보드에서 문자 한개 입력받기

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
- int GetData(void *p_data,char type)
- {
-    int result = 0;
-    if(type == 1) result = *(char *)p_data;
-    else if(type == 2)result = *(short *)p_data;
-    else if(type == 4) result = *(int *)p_data;
-     return result;
- }
  int main()
  {
-    int data = 0x12345678;
-     printf("%X\n", GetData(&data,2));
+    int input_data;
+    input_data = getchar();
+     printf("input : %c\n",input_data);
  }

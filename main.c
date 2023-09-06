9fdb3346cbd4e758dead42f2a48550579bf87d65
juhanbin <gksqls0509@naver.com>
2023-09-06 16:19:38 +0900

    fget 함수로 문자열 입력 받을때 입력 취소까지 처리하기

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
  int main()
  {
     char input_string[10];
-    fgets(input_string,10,stdin);
-     printf("input : %s\n",input_string);
+    if(NULL != fgets(input_string,10,stdin))
+    {
+        printf("input : %s\n",input_string);
+    }
+    else
+    {
+        printf("input -> Canceled");
+    }
  }

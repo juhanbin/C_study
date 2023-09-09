b1fe902bff6b1f695b244f7b6276731733caf6ac
juhanbin <gksqls0509@naver.com>
2023-09-09 18:47:52 +0900

    길이 제한 없이 문자열 형식의 숫자를 정수로 변환하기(2)

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 
  int main()
  {
-    int pos_num = 1, num = 0, i ,count;
+    int num = 0, count = 0;
     char num_string[4] = "123";
 
-    count = strlen(num_string);
-    for(i = 0;i<count;i++)
-        pos_num = pos_num * 10;
-
-    for(i = 0;i<count;i++)
-    {
-        num = num + (num_string[i]-'0') * pos_num;
-        pos_num = pos_num / 10;
-    }
-     printf("%s -> %d\n",num_string,num);
+     while (num_string[count] != 0)
+     {
+         num = num * 10 + (num_string[count] - '0');
+         count++;
+     }
+     printf("%s -> %d\n", num_string,num);
  }

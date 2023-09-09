ab67c024a0be8f2ef747bc325ca1c9f54aa7bb4b
juhanbin <gksqls0509@naver.com>
2023-09-09 18:58:51 +0900

    gets 함수를 사용하여 두개의 숫자를 입력 받아 합산하기

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 
+ int ArrayToInteger(char string[])
+ {
+    int count = 0, num = 0;
+    while(string[count] != 0)
+    {
+        num = num * 10 + string[count] - '0';
+        count ++;
+    }
+     return num;
+ }
  int main()
  {
-    int num = 0, count = 0;
-    char num_string[4] = "123";
+    int first_num, second_num;
+    char first_string[16],second_string[16];
 
-     while (num_string[count] != 0)
-     {
-         num = num * 10 + (num_string[count] - '0');
-         count++;
-     }
-     printf("%s -> %d\n", num_string,num);
+     printf("input first number : ");
+     gets(first_string);
+     printf("input second number : ");
+     gets(second_string);
+
+     first_num = ArrayToInteger(first_string);
+     second_num = ArrayToInteger(second_string);
+     printf("%d + %d = %d\n",first_num,second_num,first_num+second_num);
  }

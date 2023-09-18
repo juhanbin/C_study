77aa4065d9fc3b610f44f31135842f1a874802fd
juhanbin <gksqls0509@naver.com>
2023-09-18 11:31:09 +0900

    구조체를 사용해서 사람의 신체 정보를 입력 받고 출력하기

--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
-typedef unsigned short int US;
+typedef struct People
+{
+    char name[12];
+    unsigned short int age;
+    float height;
+    float weight;
+}Person;
+
 int main()
 {
-    unsigned short int data = 5;
-    US temp;
+   Person data;
 
-    temp = data;
-    printf("tmep = %d\n",temp);
+    printf("대상자의 정보를 입력하세요\n");
+    printf("이름 : ");
+    scanf("%s",&data.name);
+    printf("나이 : ");
+    scanf("%hu",&data.age);
+    printf("키 : ");
+    scanf("%f",&data.height);
+    printf("몸무게 : ");
+    scanf("%f",&data.weight);
+
+    printf("\n입력한 정보는 다음과 같습니다\n");
+    printf("%s : %d세, %.1fcm, %1.fkg\n",data.name,data.age,data.height,data.weight);
 }

853da7e337bbc5654327adf9e9044db8805fe52c
juhanbin <gksqls0509@naver.com>
2023-09-29 20:01:50 +0900

    Sum 함수 호출하기

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    int money;
-    money = 3000;
-    printf("money에 저장된 값은 %d",money);
+int Sum(int value1, int value2)
+{
+    int result = value1 + value2;
+    return result;
+}
+int main()
+{
+    int s;
+    s = Sum(2,3);
+    printf("더하기 작업의 결과는 %d\n",s);
 }

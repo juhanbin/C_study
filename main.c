f9da8c7b102eb97c9428a193f85936add3733341
juhanbin <gksqls0509@naver.com>
2023-10-12 23:31:04 +0900

    switch문을 사용하여 점수에 따른 등급 지정하기

--- main.c
+++ main.c
@@ -5,24 +5,24 @@ int main()
     int score = 86;
     char grade;
 
-    if (score >=90)
-        grade = 'A';
-    else
+    switch(score/10)
     {
-        if(score >= 80)
+        case 10 :
+        case 9:
+            grade = 'A';
+            break;
+        case 8:
             grade = 'B';
-        else
-        {
-            if(score >= 70)
-                grade = 'C';
-            else
-            {
-                if(score >= 60)
-                    grade = 'D';
-                else
-                    grade = 'F';
-            }
-        }
+            break;
+        case 7:
+            grade = 'C';
+            break;
+        case 6:
+            grade = 'D';
+            break;
+        default:
+            grade = 'F';
+            break;
     }
-    printf("당신의 점수는 %d점이고 등급은 %c입니다.\n",score,grade);
+    printf("당신의 점수는 %d점이고 등급은 %c입니다",score,grade);
 }

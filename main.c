10c5527258edb099cb5d2c46ae3f6051cb8eac7b
juhanbin <gksqls0509@naver.com>
2023-10-12 20:18:04 +0900

    대입 연산자를 이용하여 변수에 값 저장하기

--- main.c
+++ main.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-int Sum(int value1, int value2)
-{
-    int result = value1 + value2;
-    return result;
-}
 int main()
 {
-    int s;
-    s = Sum(2,3);
-    printf("더하기 작업의 결과는 %d\n",s);
+    int data1, data2;
+    data1 = 5;
+    data2 = data1;
+    printf("data1 = %d, data2 = %d\n",data1,data2);
 }

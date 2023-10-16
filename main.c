0f4bbd8cbd4b9a9cd1793aa86adb5ef34feae746
juhanbin <gksqls0509@naver.com>
2023-10-16 20:19:12 +0900

    for반복문과 함수를 사용하여 구구단 5단 출력하기

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void ShowMultiplication()
+void ShowMultiplication(int step)
 {
     int i;
     for(i=1;i<=9;i++)
     {
-        printf("2 * %d = %d\n",i,i*2);
+        printf("%d * %d = %d\n",step,i,i*2);
     }
 }
 int main()
 {
-    ShowMultiplication();
+    ShowMultiplication(5);
 }

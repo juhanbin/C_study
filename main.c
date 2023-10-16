730987ed18d2e2dec688c45b32199bd735c201a7
juhanbin <gksqls0509@naver.com>
2023-10-16 20:21:09 +0900

    for반복문과 함수를 사용하여 구구단 전체 출력하기

--- main.c
+++ main.c
@@ -5,10 +5,12 @@ void ShowMultiplication(int step)
     int i;
     for(i=1;i<=9;i++)
     {
-        printf("%d * %d = %d\n",step,i,i*2);
+        printf("%d * %d = %d\n",step,i,i * step);
     }
 }
 int main()
 {
-    ShowMultiplication(5);
+    int m;
+    for(m=2;m<=9;m++)
+    ShowMultiplication(m);
 }

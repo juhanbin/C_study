203b4414d0cc9a848923782917aeac5251c630b3
juhanbin <gksqls0509@naver.com>
2023-10-16 20:35:53 +0900

    goto문 써보기

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 int main()
 {
     int m, n;
-    for(m = 5; m < 8; m++)
+    for(m = 5; m <7; m++)
     {
-        if(m == 6)
-            continue;
-        printf("m(%d)\n",m);
+        for(n = 0; n<3; n++)
+        {
+            if(m == 5 && n==1)
+                goto exit_pos;
+            printf("m(%d)-n(%d)\n",m,n);
+        }
     }
+    exit_pos :
+    printf("<end>\n",m,n);
 }

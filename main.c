1add59a0e82316c7f7f4736483430f734e72503a
juhanbin <gksqls0509@naver.com>
2023-10-16 20:18:09 +0900

    for반복문과 함수를 사용하여 구구단 2단 출력하기

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main()
+void ShowMultiplication()
 {
     int i;
     for(i=1;i<=9;i++)
     {
-        printf("2 * %d\n",i,2*i);
+        printf("2 * %d = %d\n",i,i*2);
     }
+}
+int main()
+{
+    ShowMultiplication();
 }

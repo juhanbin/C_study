55e1cac5cd9422a7d374f3635d4badf8282df09d
juhanbin <gksqls0509@naver.com>
2023-10-16 20:32:35 +0900

    continue문을 사용해서 반복문의 실행 흐름 변경하기

--- main.c
+++ main.c
@@ -3,15 +3,10 @@
 int main()
 {
     int m, n;
-    for(m = 5; m < 7; m++)
+    for(m = 5; m < 8; m++)
     {
-        for(n =0; n<3; n++)
-        {
-            if(m == 5 && n == 1)
-                break;
-            printf("m(%d) - n(%d)\n",m,n);
-        }
-        if(m == 5 && n == 1)
-            break;
+        if(m == 6)
+            continue;
+        printf("m(%d)\n",m);
     }
 }

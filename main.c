69f20a1538339e9aae82632960833431aeb0a46a
juhanbin <gksqls0509@naver.com>
2023-10-18 23:33:41 +0900

    변수의 특정 비트를 0으로 만드는 예제

--- main.c
+++ main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
+unsigned char ResetBit(unsigned char dest_data, unsigned char bit_num)
+{
+    if(bit_num < 8)
+        dest_data = dest_data = dest_data & ~(0x01 << bit_num);
+    return dest_data;
+}
 int main()
 {
-    int m, n;
-    for(m = 5; m <7; m++)
-    {
-        for(n = 0; n<3; n++)
-        {
-            if(m == 5 && n==1)
-                goto exit_pos;
-            printf("m(%d)-n(%d)\n",m,n);
-        }
-    }
-    exit_pos :
-    printf("<end>\n",m,n);
+    unsigned char lamp_state = 0x7f;
+    printf("%X->",lamp_state);
+    lamp_state = ResetBit(lamp_state,3);
+    printf("%X\n",lamp_state);
 }

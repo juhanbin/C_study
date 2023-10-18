e464ab2accf65d1273d8c151aa604c1e04755906
juhanbin <gksqls0509@naver.com>
2023-10-18 23:46:24 +0900

    변수의 값을 2진수 형태로 확인하는 예제

--- main.c
+++ main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-unsigned char SetBit(unsigned char dest_data, unsigned char bit_num)
+unsigned char GetBit(unsigned char dest_data, unsigned char bit_num)
 {
+    unsigned char bit_state = 0;
     if(bit_num < 8)
-        dest_data = dest_data = dest_data | (0x01 << bit_num);
-    return dest_data;
+    {
+        bit_state = dest_data & (0x01 << bit_num);
+        bit_state = bit_state >> bit_num;
+    }
+    return bit_state;
 }
 int main()
 {
-    unsigned char lamp_state = 0x77;
+    unsigned char lamp_state = 0x75;
+    unsigned char bit_state;
+    int i;
     printf("%X->",lamp_state);
-    lamp_state = SetBit(lamp_state,3);
-    printf("%X\n",lamp_state);
+    for(i=0;i<8;i++)
+    {
+        bit_state = GetBit(lamp_state, 7-i);
+        printf("%d",bit_state);
+    }
+    printf("\n");
 }
